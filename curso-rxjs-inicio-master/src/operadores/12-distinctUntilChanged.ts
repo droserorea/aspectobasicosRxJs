02e41d2530cf75fb5fefad7e5aa103ea6cf574cf
drosero <droserorea@gmail.com>
2021-05-08 17:56:41 -0500

    Op12-distinctUntilChanged

--- curso-rxjs-inicio-master/src/operadores/12-distinctUntilChanged.ts
+++ curso-rxjs-inicio-master/src/operadores/12-distinctUntilChanged.ts
@@ -4,6 +4,14 @@
 import { from, of } from "rxjs";
 import { distinct, distinctUntilChanged } from "rxjs/operators";
 
+const numeros$=of<number|string>(1,'1',1,1,3,3,3,2,2,2,5,4,3,1);
+//la emision de string o number identido si se separa como 
+//emisiones independientes
+
+numeros$.pipe(
+    distinctUntilChanged()
+).subscribe(console.log);
+
 interface Personaje{
     nombre:string;
 }
