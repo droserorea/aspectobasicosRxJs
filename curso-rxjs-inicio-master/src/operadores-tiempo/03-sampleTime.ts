787642282fdf62302cfe31db1c32b2df2fc7af2e
drosero <droserorea@gmail.com>
2021-05-08 19:23:30 -0500

    OT-03-sampleTime

---
+++ curso-rxjs-inicio-master/src/operadores-tiempo/03-sampleTime.ts
@@ -0,0 +1,13 @@
+//sampleTime obtener el ultimo valor emitido en un intervalo de tiempo
+//permite estar pendiente de las subscripciones en el lapso del tiempo
+
+import { fromEvent } from "rxjs";
+import { map, sampleTime } from "rxjs/operators";
+
+const click$=fromEvent<MouseEvent>(document,'click');
+
+click$.pipe(
+    sampleTime(3000),
+    map(({x,y})=>({x,y})),
+    //espera el tiempo y luego    
+).subscribe(console.log);
